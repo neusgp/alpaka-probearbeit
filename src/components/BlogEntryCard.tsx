2f0d980f54f8650926269336434902c8e839aba0
Neus Garcia Puigdollers <neusgp@Neuss-MBP.fritz.box>
2025-03-26 09:40:10 +0100

    fix: photo url

--- src/components/BlogEntryCard.tsx
+++ src/components/BlogEntryCard.tsx
@@ -3,7 +3,7 @@ import { Button } from "./Button";
 export const BlogEntryCard = () => {
   return (
     <div
-      className={`aspect-3/4 min-w-[294.5px] bg-[url(7.png)] bg-center rounded-md flex flex-col justify-end items-center pb-10`}>
+      className={`aspect-3/4 min-w-[294.5px] bg-[url(/7.png)] bg-center rounded-md flex flex-col justify-end items-center pb-10`}>
       <Button
         label="Lorem Ipsum"
         intent="gray"
